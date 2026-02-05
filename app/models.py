8f9c91bbdf70a5076c2a93bdd812585a0d6d1518
marwadicoder <143351798+marwadicoder@users.noreply.github.com>
2026-02-05 19:20:54 +0530

    Updated views, templates, models, and static files

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Caterer(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='caterers/')
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Caterer
 
 def home(request):
-    return render(request, 'index.html')
-
+    caterers = Caterer.objects.all()
+    return render(request, "index.html", {
+        "caterers": caterers
+    })
 def about(request):
     return render(request, 'about.html')
 
